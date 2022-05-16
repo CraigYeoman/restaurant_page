beab9bc0207aea593b3b472a3eb0a6902037550d
CraigYeoman <yeomancraig@gmail.com>
2022-05-16 07:22:06 -0500

    Reworking webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development'.at,
-  entry: {
-    index: './src/index.js',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development'
-    }),
-  ],
-  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
-  optimization: {
-    runtimeChunk: 'single',
-  }
 };
